c03026ae36ff788988bbb43a6cb7671d39cf57a9
denpolandd <denpolandd@gmail.com>
2023-09-19 21:19:42 +0300

    задача про степени тройки

---
+++ practice/first.cs
@@ -0,0 +1,17 @@
+﻿int num;
+num = int.Parse(Console.ReadLine());
+
+static string IsPowOfThree(int n)
+{
+    for (int i = 1; i <= n; i *= 3)
+    {
+        if (n == i)
+        {
+            return ("Число является степенью тройки");
+        }
+    }
+
+    return ("Число не является степенью тройки");
+}
+
+Console.WriteLine(IsPowOfThree(num));
