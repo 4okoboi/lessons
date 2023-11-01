a2fca6d96b2d070e553a23093eb594fdfb2b1f71
4okoboi <74819042+4okoboi@users.noreply.github.com>
2023-11-01 12:38:25 +0300

    Create wordsDictionary.cs

---
+++ wordsDictionary.cs
@@ -0,0 +1,39 @@
+static void Main()
+{
+    // Создаем словарь для хранения слов и их частот
+    Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+    // Замените "путь_к_вашему_файлу.txt" на актуальный путь к вашему текстовому файлу
+    string filePath = "путь_к_вашему_файлу.txt";
+
+
+    // Считываем текст из файла
+    string text = File.ReadAllText(filePath);
+
+    // Разбиваем текст на слова, используя регулярное выражение
+    string[] words = Regex.Split(text, @"\W+");
+
+    // Подсчитываем частоту каждого слова
+    foreach (string word in words)
+    {
+        if (!string.IsNullOrEmpty(word))
+        {
+            string lowercaseWord = word.ToLower(); // Приводим слово к нижнему регистру
+            if (wordFrequency.ContainsKey(lowercaseWord))
+            {
+                wordFrequency[lowercaseWord]++;
+            }
+            else
+            {
+                wordFrequency[lowercaseWord] = 1;
+            }
+        }
+    }
+
+    // Выводим результаты
+    foreach (var kvp in wordFrequency)
+    {
+        Console.WriteLine($"Слово: {kvp.Key}, Повторений: {kvp.Value}");
+    }
+    
+}
